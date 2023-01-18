49483d9d21ca33a68d6090d333fc976d45fc3638
TCHANDA-ervan <94933948+TCHANDA-ervan@users.noreply.github.com>
2023-01-18 16:08:41 +0100

    tchanda-ervan
    project test logiciel

---
+++ tests/integrationTests/ContactServiceIntegrationTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+require __DIR__.'/../../src/ContactService.php';
+
+/**
+ * * @covers invalidInputException
+ * @covers \ContactService
+ *
+ * @internal
+ */
+final class ContactServiceIntegrationTest extends TestCase
+{
+    private $contactService;
+
+    public function __construct(string $name = null, array $data = [], $dataName = '') {
+        parent::__construct($name, $data, $dataName);
+        $this->contactService = new ContactService();
+    }
+
+    // test de suppression de toute les données, nécessaire pour nettoyer la bdd de tests à la fin
+    public function testDeleteAll()
+    {
+        $contactService = new ContactService();
+        $contactService->deleteAllContact();
+    }
+
+
+    public function testCreationContact()
+    {
+        $contactService = new ContactService();
+        $contactService->createContact("pascal","victor");
+        //du principe que id_pascal=2
+        if($contactService->searchContact(2))
+        {
+            throw new Exception("l'utilisateur a bien ete cree");
+        }
+        else
+        {
+            throw new Exception("l'utilisateur na pas ete cree");
+        }
+    }
+
+    public function testSearchContact()
+    {
+        $contactService = new ContactService();
+        $contactService->searchContact(2);
+    }
+
+    public function testModifyContact()
+    {
+        $contactService = new ContactService();
+        $contactService->createContact(2,"pascal","victor");
+    }
+
+    public function testDeleteContact()
+    {
+        $contactService = new ContactService();
+        $contactService->deleteContact(2);
+    }
+
+}
---
+++ tests/unitTests/ContactServiceUnitTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/../../src/ContactService.php';
+
+/**
+ * * @covers invalidInputException
+ * @covers \ContactService
+ *
+ * @internal
+ */
+final class ContactServiceUnitTest extends TestCase {
+    private $contactService;
+
+    public function __construct(string $name = null, array $data = [], $dataName = '') {
+        parent::__construct($name, $data, $dataName);
+        $this->contactService = new ContactService();
+    }
+
+    // //test Création Contact Sans Texte ok
+    // public function testCreationContactWithoutAnyText() {
+    //     $this->expectException(Exception::class);
+    //     $this->expectExceptionMessage("le nom  doit être renseigné");
+    //     $contactService = new ContactService();
+    //     $contactService->createContact(null,null);
+    // }
+
+    //test Création Contact Sans Prenom
+    public function testCreationContactWithoutPrenom() {
+        
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('le prenom doit être renseigné');
+        $contactService = new ContactService();
+        $contactService->createContact("wenji",null);
+    }
+    
+    //test Création Contact Sans Prenom
+    public function testCreationContactWithoutNom() {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('le nom  doit être renseigné');
+        $contactService = new ContactService();
+        $contactService->createContact(null,"pascal");
+    }
+    //test Rechercher un contact avec un numéro
+    public function testSearchContactWithNumber() {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('search doit être une chaine de caractères');
+        $contactService = new ContactService();
+        $contactService->searchContact(2);
+    }
+    //test Rechercher un contactSans Texte ok
+    public function testSearchContactWithAnyText()
+    {   
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('search doit être renseigné');
+        $contactService = new ContactService();
+        $contactService->searchContact("");
+    }
+
+    //ok
+    public function testModifyContactWithInvalidId() {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("l'id doit être un entier non nul");
+        $contactService = new ContactService();
+        $contactService->UpdateContact(-5,"pascal","loic");
+    }
+
+    //test Supprimer le contact avec le texte comme identifiant ok
+    public function testDeleteContactWithTextAsId() {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("l'id doit être un entier non nul");
+        $contactService = new ContactService();
+        $contactService->deleteContact("zozo");
+    }
+}
